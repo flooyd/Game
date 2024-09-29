4162b6c7acea2898a1315462c934549c51aa8bd6
floyd <floydtjones@gmail.com>
2024-09-29 01:13:37 -0500

    update url

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -127,7 +127,7 @@
 		window.addEventListener('keypress', handleKeyPress);
 
 		// Connect to the server
-		socket = io('wss://ec2-18-117-172-171.us-east-2.compute.amazonaws.com:443');
+		socket = io('https://gameserver-6052.onrender.com');
 
 		socket.on('connect', () => {
 			console.log('Connected to server');
