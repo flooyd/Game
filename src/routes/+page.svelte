93122bddcb3a03f83d6f01e019efd3de258125ba
floyd <floydtjones@gmail.com>
2024-10-08 22:35:11 -0500

    interp

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -214,7 +214,7 @@
 		}
 
 		if (player) {
-			if (time - lastPlayerUpdate >= 1000 / 60 && shouldUpdatePlayer) {
+			if (time - lastPlayerUpdate >= 1000 / 144 && shouldUpdatePlayer) {
 				lastPlayerUpdate = time;
 				$socket?.emit('PlayerMove', player);
 				shouldUpdatePlayer = false;
